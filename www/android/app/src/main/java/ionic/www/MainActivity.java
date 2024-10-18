1442471f911d9a61b436e7ef15be4c121aaa3f8b
Chao <pkh_chao@photons.com.tw>
2024-10-18 18:28:53 +0800

    init Ionic

---
+++ www/android/app/src/main/java/ionic/www/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.www;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ www/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.www',
+  appName: 'www',
+  webDir: 'dist'
+};
+
+export default config;
